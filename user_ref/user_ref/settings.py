6c2f0175ebc6f1bd00378db9a35f97aec26397cf
Artem <artsefimov@gmail.com>
2025-07-26 00:55:59 +0300

    add dckr

--- user_ref/user_ref/settings.py
+++ user_ref/user_ref/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'user_ref.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ref_db',
+        'USER': 'ref_user',
+        'PASSWORD': 'ref',
+        'HOST': 'localhost',
+        'PORT': '5432',
     },
     'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
